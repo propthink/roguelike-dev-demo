44b4137074febee4ea97134b9f9a3909250e1a36
Joe <joewfanelli@gmail.com>
2024-07-17 19:18:29 -0400

    added logic for increasing difficulty across floors

--- proc_gen.py
+++ proc_gen.py
@@ -1,7 +1,7 @@
 # import dependencies
 from __future__ import annotations
 import random
-from typing import Iterator, List, Tuple, TYPE_CHECKING
+from typing import Dict, Iterator, List, Tuple, TYPE_CHECKING
 import tcod
 
 import entity_factories
@@ -12,18 +12,30 @@ if TYPE_CHECKING:
     from engine import Engine
     from entity import Entity
 
-#
 max_items_by_floor = [
     ( 1, 1 ),
     ( 4, 2 )
 ]
 
-#
 max_monsters_by_floor = [
     ( 1, 2 ),
     ( 4, 3 ),
     ( 6, 5 )
 ]
+
+item_chances: Dict[int, List[Tuple[Entity, int]]] = {
+    0: [(entity_factories.health_potion, 35)],
+    2: [(entity_factories.confusion_scroll, 10)],
+    4: [(entity_factories.lightning_scroll, 25)],
+    6: [(entity_factories.fireball_scroll, 25)]
+}
+
+enemy_chances: Dict[int, List[Tuple[Entity, int]]] = {
+    0: [(entity_factories.orc, 80)],
+    3: [(entity_factories.troll, 15)],
+    5: [(entity_factories.troll, 30)],
+    7: [(entity_factories.troll, 60)]
+}
 
 #
 def get_max_value_for_floor(
@@ -43,6 +55,38 @@ def get_max_value_for_floor(
             current_value = value
 
     return current_value
+
+#
+def get_entities_at_random(
+    weighted_chances_by_floor: Dict[int, List[Tuple[Entity, int]]],
+    number_of_entities: int,
+    floor: int
+) -> List[Entity]:
+    
+    entity_weighted_chances = {}
+
+    for key, values in weighted_chances_by_floor.items():
+
+        if key > floor:
+
+            break
+
+        else:
+
+            for value in values:
+
+                entity = value[0]
+                weighted_chance = value[1]
+
+                entity_weighted_chances[entity] = weighted_chance
+
+    entities = list( entity_weighted_chances.keys() )
+    entity_weighted_chance_values = list( entity_weighted_chances.values() )
+
+    chosen_entities = random.choices(
+        entities, weights=entity_weighted_chance_values, k=number_of_entities
+    )
+    return chosen_entities
 
 #
 class RectangularRoom:
@@ -91,42 +135,19 @@ def place_entities( room: RectangularRoom, dungeon: GameMap, floor_number: int )
         0, get_max_value_for_floor( max_items_by_floor, floor_number )
     )
 
-    # step through each possible monster
-    for i in range( number_of_monsters ):
+    monsters: List[Entity] = get_entities_at_random(
+        enemy_chances, number_of_monsters, floor_number
+    )
+    items: List[Entity] = get_entities_at_random(
+        item_chances, number_of_items, floor_number
+    )
 
-        # generate the location of the monster in the current room
-        x = random.randint( room.x1 + 1, room.x2 - 1 )
-        y = random.randint( room.y1 + 1, room.y2 - 1 )
-
-        # only place a new entity if the current tile is empty
-        if not any( entity.x == x and entity.y == y for entity in dungeon.entities ):
-
-            if random.random() < 0.8:
-
-                # generate an orc
-                entity_factories.orc.spawn( dungeon, x, y )
-
-            else:
-
-                # generate a troll
-                entity_factories.troll.spawn( dungeon, x, y )
-
-        # step through each item
-        for i in range( number_of_items ):
+    for entity in monsters + items:
             x = random.randint( room.x1 + 1, room.x2 - 1 )
             y = random.randint( room.y1 + 1, room.y2 - 1 )
 
             if not any( entity.x == x and entity.y == y for entity in dungeon.entities ):
-                item_chance = random.random()
-
-                if item_chance < 0.7:
-                    entity_factories.health_potion.spawn( dungeon, x, y )
-                elif item_chance < 0.8:
-                    entity_factories.fireball_scroll.spawn( dungeon, x, y )
-                elif item_chance < 0.9:
-                    entity_factories.confusion_scroll.spawn( dungeon, x, y )
-                else:
-                    entity_factories.lightning_scroll.spawn( dungeon, x, y )
+                entity.spawn( dungeon, x, y )
     
 # builds an L-shaped tunnel between two points
 def tunnel_between( 
